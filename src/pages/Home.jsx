87bd9b7349a6f4a254901b208df32986b596b989
Mouhssine Oubardan <mouhssine.oubardan@hotmail.com>
2022-04-06 15:45:01 +0000

    Add shopping product list ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
+import Products from "../components/Products";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
     </div>
   );
 }
