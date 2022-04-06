cb93b1abfb99d3704e786ffc020f10a1bdda308e
Mouhssine Oubardan <mouhssine.oubardan@hotmail.com>
2022-04-06 14:47:51 +0000

    Add shopping categories design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Slider from '../components/Slider';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
 
 function Home() {
   return (
@@ -9,8 +10,9 @@ function Home() {
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
