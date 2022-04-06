8ffd0029bb894439d9c89328fd5e255bc381ae90
Mouhssine Oubardan <mouhssine.oubardan@hotmail.com>
2022-04-06 13:41:24 +0000

    create pure react slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider';
+import Announcement from "../components/Announcement";
 
 function Home() {
   return (
     <div>
+      <Announcement />
       <Navbar />
+      <Slider />
     </div>
   )
 }
