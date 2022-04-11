e1f72c30ccde742cc93cccc545a6a7844df58784
Mouhssine Oubardan <mouhssine.oubardan@hotmail.com>
2022-04-11 13:36:08 +0000

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Card from "./pages/Card";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Pay from "./pages/Pay";
+import Success from "./pages/Success";
 
 function App() {
+  const user = true;
   return (
-    <div>
-      <Card />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/products/:category" element={<ProductList />} />
+      <Route
+        path="/register"
+        element={user ? <Navigate to="/" /> : <Register />}
+      />
+      <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+      <Route path="/card" element={<Card />} />
+      <Route path="/pay" element={<Pay />} />
+      <Route path="/success" element={<Success />} />
+    </Routes>
   );
 }
 
